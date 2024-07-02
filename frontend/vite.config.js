b9cceabfcabf455106610a76f4cd193f7e5865c8
sushilkumar666 <sushilssharma064@gmail.com>
2024-07-02 17:13:42 +0530

    config file changess

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Since your index.html is at the root
+  build: {
+    outDir: 'dist', // Where the production build will be generated
+  },
+  server: {
+    open: true,
+  },
 })
