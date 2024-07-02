301c449e82bade1af9facf7324cc117b19fd963b
Sushilkumar Sharma <73883613+sushilkumar666@users.noreply.github.com>
2024-07-02 11:49:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ dotenv.config({ path: './.env' });
 const app = express();
 
 app.use(cors({
-    origin: 'https://virtual-hospital-cdnr.vercel.app/', // Update with your frontend URL
+    origin: 'https://virtual-hospital-cdnr.vercel.app', // Update with your frontend URL
     credentials: true,
 }));
 app.use(express.json({ limit: "16kb" }));
