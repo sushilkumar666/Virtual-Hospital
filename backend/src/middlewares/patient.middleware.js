fbe8f9cae12aeb6e26a9d838bcd30949806e35b0
sushilkumar666 <sushilssharma064@gmail.com>
2024-07-24 12:49:27 +0530

    cc

--- backend/src/middlewares/patient.middleware.js
+++ backend/src/middlewares/patient.middleware.js
@@ -5,7 +5,7 @@ import { ApiError } from "../utils/ApiError.js";
 export const verifyJWT = async (req, res, next) => {
     try {
         const token = req?.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-         console.log(" this si stoken sushil "  + req?.cookies)
+        console.log(token, "Token Coming" + " req.cookies " + req?.cookies );
         if (!token) {
             console.log("Token not found in cookies or headers.");
             throw new ApiError(401, "Unauthorized request");
