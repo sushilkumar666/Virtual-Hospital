40a21af57992e7529976d9de33e0b386d1e96552
sushilkumar666 <sushilssharma064@gmail.com>
2024-07-24 12:45:03 +0530

    cc

--- backend/src/middlewares/doctor.middleware.js
+++ backend/src/middlewares/doctor.middleware.js
@@ -9,7 +9,7 @@ export const verifyJWT = async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
-         console.log(token, "Token Coming");
+         console.log(token, "Token Coming" + " req.cookies " + req?.cookies );
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
