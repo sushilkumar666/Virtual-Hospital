75dfcef5bfe912ca6ce5091070711e81a65f3443
sushilkumar666 <sushilssharma064@gmail.com>
2024-10-26 16:18:29 +0530

    cc

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,14 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-    origin: 'https://virtual-hospital-frontend.vercel.app', // Update with your frontend URL
+    origin: ['https://virtual-hospital-frontend.vercel.app', 'http://localhost:5173'], // Array of allowed origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204,
 };
 
 app.use(cookieParser());
-app.use(cors(corsOptions)); // Use cors middleware
+// app.use(cors(corsOptions)); 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
