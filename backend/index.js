4b69d48df012203c4c593224213f009e0cbcc3be
Sushilkumar Sharma <73883613+sushilkumar666@users.noreply.github.com>
2024-10-26 22:08:45 +0530

    cc

--- backend/index.js
+++ backend/index.js
@@ -25,14 +25,15 @@ const corsOptions = {
 
 
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 
 // Additional CORS headers for all routes
- 
+
 app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
         return res.sendStatus(204);
