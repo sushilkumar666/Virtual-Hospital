3453c46cbfa408d55ff0d2e636f19fabc3f681ac
sushilkumar666 <sushilssharma064@gmail.com>
2024-10-26 16:30:34 +0530

    uiu

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(cookieParser());
-// app.use(cors(corsOptions)); 
+app.use(cors(corsOptions)); 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
